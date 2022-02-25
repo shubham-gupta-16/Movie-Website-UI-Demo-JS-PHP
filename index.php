0665ae0c4f343f238bec4c325034c96c7ae8ae39
Shubham Gupta <shubham.developer16@gmail.com>
2022-02-25 11:07:02 +0530

    Title Fixed

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ die(); */
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Download Page</title>
+    <title>Master Page</title>
     <style>
         * {
             box-sizing: border-box;
